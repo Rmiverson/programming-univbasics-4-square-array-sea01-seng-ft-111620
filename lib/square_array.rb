32c54e314b769da6dbc73b3498b20cd1d5037a32
Riley Iverson <rmiverson99@gmail.com>
2020-10-20 22:44:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,11 @@
 def square_array(array)
   i = array.length
-  new_array = []
   
   while i > 0 do
     i -= 1
     
     array[i] = array[i] ** 2
     
-    puts new_array[i]
+    puts array[i]
   end
 end
